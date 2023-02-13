8f263be93b037a542648def2a1d43fe39adac701
River Jiang <2126+River@users.noreply.github.com>
2023-02-13 14:38:24 -0800

    update gitignore and import rest of fiels

--- scripts/extract_ecg_xml.py
+++ scripts/extract_ecg_xml.py
@@ -7,61 +7,63 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from scipy import signal
-
+import argparse
 import untangle
 
+
 class ECGXMLExtract:
-	def __init__(self, path):
-		self.path = path
-		self.lead_voltages = None
-		self.lead_names = None
+    def __init__(self, path):
+        self.path = path
+        self.lead_voltages = None
+        self.lead_names = None
 
-		try:
-			self.ecg = untangle.parse(path)
+        try:
+            self.ecg = untangle.parse(path)
 
-			self.patient_demographics = self.ecg.RestingECG.PatientDemographics
-			self.test_demographics = self.ecg.RestingECG.TestDemographics
-			self.resting_ecg_measurements = self.ecg.RestingECG.RestingECGMeasurements
-			
-			# Waveform[0] is the "median" beat
-			# Waveform[1] is the "rhythm" waveform
-			self.waveform = self.ecg.RestingECG.Waveform[1]
+            self.patient_demographics = self.ecg.RestingECG.PatientDemographics
+            self.test_demographics = self.ecg.RestingECG.TestDemographics
+            self.resting_ecg_measurements = self.ecg.RestingECG.RestingECGMeasurements
 
-			self.lead_voltages = self.makeLeadVoltages()
-			self.lead_names = self.lead_voltages.columns
+            # Waveform[0] is the "median" beat
+            # Waveform[1] is the "rhythm" waveform
+            self.waveform = self.ecg.RestingECG.Waveform[1]
 
-		except Exception as e:
-			print("Unable to parse: "+path+' '+str(e))
+            self.lead_voltages = self.makeLeadVoltages()
+            self.lead_names = self.lead_voltages.columns
 
-	def makeLeadVoltages(self):
-		num_leads = 0
-		leads = pd.DataFrame()
+        except Exception as e:
+            print("Unable to parse: " + path + " " + str(e))
 
-		for lead in self.waveform.LeadData:
-			num_leads += 1
+    def makeLeadVoltages(self):
+        num_leads = 0
+        leads = pd.DataFrame()
 
-			lead_data = lead.WaveFormData.cdata
-			lead_b64 = base64.b64decode(lead_data)
-			# 'h' indicates each byte represents a signed short int
-			# see 'type codes' in the array documentation
-			lead_vals = np.array(array.array('h', lead_b64))
+        for lead in self.waveform.LeadData:
+            num_leads += 1
 
-			leads[lead.LeadID.cdata] = lead_vals
+            lead_data = lead.WaveFormData.cdata
+            lead_b64 = base64.b64decode(lead_data)
+            # 'h' indicates each byte represents a signed short int
+            # see 'type codes' in the array documentation
+            lead_vals = np.array(array.array("h", lead_b64))
 
-		return leads
+            leads[lead.LeadID.cdata] = lead_vals
 
-	def getVoltages(self):
-		return self.lead_voltages
+        return leads
 
-	def getVoltagesNp(self):
-		return self.lead_voltages.to_numpy()
+    def getVoltages(self):
+        return self.lead_voltages
+
+    def getVoltagesNp(self):
+        return self.lead_voltages.to_numpy()
+
 
 def preprocess_leads(leads, samples):
-    '''
+    """
     Preprocess ECG Data (input: numpy array dim(n,8))
     1. Resample data to 250 Hz (source data is 250-500 Hz)
     2. Normalize values to have mean 0 and std 1
-    '''
+    """
 
     # scale to mean=0 and std=1
     scalar = StandardScaler(with_mean=True, with_std=True)
@@ -73,35 +75,45 @@ def preprocess_leads(leads, samples):
 
     return leads_scale_resamp
 
+
 def convert_xml_to_csv(source_dir, dest_dir, samples=2500):
-	file_names = os.listdir(source_dir)
+    file_names = os.listdir(source_dir)
 
-	for file_name in file_names:
-		try:
-			ecg = ECGXMLExtract(source_dir+'/'+file_name)
-			v = ecg.getVoltagesNp()
-			if v is not None:
-				v_n = preprocess_leads(v, samples)
-				ecg_n = pd.DataFrame(columns=ecg.lead_names, data=v_n)
+    for file_name in file_names:
+        try:
+            ecg = ECGXMLExtract(source_dir + "/" + file_name)
+            v = ecg.getVoltagesNp()
+            if v is not None:
+                v_n = preprocess_leads(v, samples)
+                ecg_n = pd.DataFrame(columns=ecg.lead_names, data=v_n)
 
-				# clip off the extension
-				save_to_path = dest_dir+'/'+file_name[0:-4]+'.csv'
-				ecg_n.to_csv(save_to_path)
-				print(f"Saved: {save_to_path}")
+                # clip off the extension
+                save_to_path = dest_dir + "/" + file_name[0:-4] + ".csv"
+                ecg_n.to_csv(save_to_path)
+                print(f"Saved: {save_to_path}")
 
-		except Exception as e:
-			print(f"Error: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
+
 
 def remove_trailing_slash(path):
-	return path[:-1] if path[-1] == '/' else path
+    return path[:-1] if path[-1] == "/" else path
+
 
 if __name__ == "__main__":
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--source_dir", help="path to ecg files (xml)", required=True)
-	parser.add_argument("--dest_dir", help="path to save csv files", required=True)
-	parser.add_argument("--samples", help="number of samples per ecg file (default 2500)", default=2500, type=int)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source_dir", help="path to ecg files (xml)", required=True)
+    parser.add_argument("--dest_dir", help="path to save csv files", required=True)
+    parser.add_argument(
+        "--samples",
+        help="number of samples per ecg file (default 2500)",
+        default=2500,
+        type=int,
+    )
+    args = parser.parse_args()
 
-	convert_xml_to_csv(remove_trailing_slash(args.source_dir), 
-		remove_trailing_slash(args.dest_dir), args.samples)
+    convert_xml_to_csv(
+        remove_trailing_slash(args.source_dir),
+        remove_trailing_slash(args.dest_dir),
+        args.samples,
+    )
---
+++ scripts/extract_ecg_xml_sierra.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+sys.path.append("../scripts")
+
+import numpy as np
+import pandas as pd
+import sierraecg
+import argparse
+
+
+class ECGXMLExtract:
+    def __init__(self, path):
+        self.path = path
+        self.lead_voltages = None
+
+        try:
+            ecg = sierraecg.read_file(path)
+            xml_leads = ecg.leads
+
+            leads_df = pd.DataFrame()
+
+            for lead in xml_leads:
+                leads_df[lead.label] = lead.samples
+
+            leads_df.drop(labels=["III", "aVR", "aVL", "aVF"], axis=1, inplace=True)
+            self.lead_voltages = leads_df
+
+        except Exception as e:
+            print("Unable to parse: " + path + " " + str(e))
+
+    def getVoltages(self, clip_end=False):
+        # clip_end: clip last 200 signals from the ecg (in Stollery files this is calibration)
+        if clip_end:
+            return self.lead_voltages.iloc[:-200, :]
+        else:
+            return self.lead_voltages
+
+
+def convert_xml_to_csv(source_dir, dest_dir):
+    file_names = os.listdir(source_dir)
+
+    for file_name in file_names:
+        # skip .DS_Store in directories
+        if file_name == ".DS_Store":
+            continue
+
+        # read the xml file
+        ecg = ECGXMLExtract(source_dir + "/" + file_name)
+        v = ecg.getVoltages(clip_end=True)
+
+        # if reading the file was successful
+        if v is not None:
+            # clip off the .xml extension and save as .csv file
+            v.to_csv(dest_dir + "/" + file_name[0:-4] + ".csv")
+            print(f"Converted: {file_name}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source_dir", help="path to ecg files (xml)")
+    parser.add_argument("--dest_dir", help="path to save csv files", required=True)
+    args = parser.parse_args()
+
+    convert_xml_to_csv(args.source_dir, args.dest_dir)
